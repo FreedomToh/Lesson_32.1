c41035e5c0d968e04061af71f736a2c73f5e888c
FreedomToh3 <freedom.toh3@gmail.com>
2021-11-08 21:47:36 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "nlohmann/json.hpp"
+#include <fstream>
+
+void writeData(nlohmann::json& data) {
+    std::string filePath = "film.json";
+    std::ofstream fileData(filePath);
+
+    fileData << data;
+}
+
+int main() {
+    nlohmann::json film;
+    film["name"] = "Interstellar";
+    film["release-date"]["China"] = "October 26, 2014";
+    film["release-date"]["US"] = "November 5, 2014";
+    film["release-date"]["Russia"] = "November 6, 2014";
+    film["release-date"]["UK"] = "November 7, 2014";
+    film["country"] = {"US", "UK"};
+    film["director"] = "Christopher Nolan";
+    film["written-by"] = {"Jonathan Nolan", "Christopher Nolan"};
+    film["producer"] = {"Emma Thomas", "Christopher Nolan", "Lynda Obst"};
+    film["starring"] = {"Matthew McConaughey", "Anne Hathaway", "Jessica Chastain",
+                        "Bill Irwin", "Ellen Burstyn", "Michael Caine"};
+    film["productions-company"] = {"Paramount Pictures", "Warner Bros. Pictures"};
+    writeData(film);
+
+    return 0;
+}
